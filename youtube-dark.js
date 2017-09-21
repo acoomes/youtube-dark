36f47767d8982703233c68154deb25e0d0f3c1ac
Andrew Coomes <86drift@gmail.com>
2017-09-20 21:20:58 -0700

    Darkened main background color, added  coloring for top bar and sidebar. Commented each piece.

--- youtube-dark.js
+++ youtube-dark.js
@@ -1,1 +1,11 @@
-document.getElementById("content").style.backgroundColor = "rgb(100, 100, 100)";
+// Set background color of the top bar
+document.getElementById("container").style.backgroundColor = "rgb(100, 100, 100)";
+
+// Set background color of the content area
+document.getElementById("content").style.backgroundColor = "rgb(80, 80, 80)";
+
+// Set background color of the sidebar
+document.getElementById("sections").style.backgroundColor = "rgb(100, 100, 100)";
+
+// Set background color of the sidebar header
+document.getElementById("guide-content").style.backgroundColor = "rgb(100, 100, 100)";
