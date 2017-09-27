56b6cadcec2ee66f9604ed42b21473a2c31f7fd4
Andrew Coomes <86drift@gmail.com>
2017-09-26 21:59:08 -0700

    Added header coloring

--- youtube-dark.js
+++ youtube-dark.js
@@ -3,6 +3,8 @@ var topBar = document.getElementById("container");
 var mainContent = document.getElementById("content");
 var sidebar = document.getElementById("sections");
 var sidebarHeader = document.getElementById("guide-content");
+var header = document.getElementById("channel-header");
+var headerTabs = document.getElementById("tabs-inner-container");
 
 // Style value variables
 var mediumGray = "rgb(100, 100, 100)";
@@ -21,3 +23,7 @@ sidebar.style.backgroundColor = mediumGray;
 
 // Style the sidebar header
 sidebarHeader.style.backgroundColor = mediumGray;
+
+// Style the header
+header.style.backgroundColor = mediumDarkGray;
+headerTabs.style.backgroundColor = mediumDarkGray;
